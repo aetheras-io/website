bc4d7bb54a4d920843c47c0cbf553772652caa58
tlee21 <teddylee@aetheras.io>
2019-06-26 17:55:46 +0800

    Add firebase hosting and remove warnings

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Button from '@material-ui/core/Button';
 import { STYLES_CONST, SHARED_STYLES } from "../utils/shared-styles";
 import { usingClasses } from "../utils/utils";
-import Grid from "@material-ui/core/Grid";
+// import Grid from "@material-ui/core/Grid";
 import { injectIntl, FormattedMessage } from 'react-intl';
 import IntlComponent from "./shared/IntlComponent";
-import CONST from "../utils/const";
+// import CONST from "../utils/const";
 
 
 class TeamMember extends IntlComponent {
@@ -25,7 +24,7 @@ class TeamMember extends IntlComponent {
         }
 
         const founderDOM = getFounderDOM(founder, classes);
-        const teamMemberDOM = getTeamMemberDOM(teamMember, classes);
+        // const teamMemberDOM = getTeamMemberDOM(teamMember, classes);
         return (
             <section className={usingClasses(classes, 'section') + usingClasses(classes, 'teamMemberSection')}>
                 <span className={usingClasses(classes, 'titleText')}>
@@ -77,27 +76,27 @@ const getFounderDOM = (founderList, classes) => {
     return founderDOM;
 }
 
-const getTeamMemberDOM = (teamMember, classes) => {
-    let teamMemberDOM = null;
-    if (teamMember && teamMember.length > 0) {
-        teamMemberDOM = teamMember.map((member, idx) => (
-            <Grid item xs={12} sm={12} md={3} lg={3} key={idx} className={usingClasses(classes, 'memberItem')}>
-                <img
-                    src={member.photo}
-                    alt="member_photo"
-                    className={usingClasses(classes, 'memberPhoto')}
-                />
-                <span className={usingClasses(classes, 'memberName')}>
-                    {member.name}
-                </span>
-                <span className={usingClasses(classes, 'memberJob')}>
-                    {member.job}
-                </span>
-            </Grid>
-        ))
-    }
-    return teamMemberDOM;
-}
+// const getTeamMemberDOM = (teamMember, classes) => {
+//     let teamMemberDOM = null;
+//     if (teamMember && teamMember.length > 0) {
+//         teamMemberDOM = teamMember.map((member, idx) => (
+//             <Grid item xs={12} sm={12} md={3} lg={3} key={idx} className={usingClasses(classes, 'memberItem')}>
+//                 <img
+//                     src={member.photo}
+//                     alt="member_photo"
+//                     className={usingClasses(classes, 'memberPhoto')}
+//                 />
+//                 <span className={usingClasses(classes, 'memberName')}>
+//                     {member.name}
+//                 </span>
+//                 <span className={usingClasses(classes, 'memberJob')}>
+//                     {member.job}
+//                 </span>
+//             </Grid>
+//         ))
+//     }
+//     return teamMemberDOM;
+// }
 
 const styles = Object.assign({ ...SHARED_STYLES }, {
     teamMemberSection: {
