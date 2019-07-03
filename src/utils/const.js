1db175663ffb8cbbcd7a27b750685b229b506d15
tlee21 <teddylee@aetheras.io>
2019-07-03 10:22:22 +0800

    Update twitter link

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,7 +1,7 @@
 const CONST = {
     githubURL: 'https://github.com/aetheras-io',
-    twitterURL: 'https://twitter.com/',
-    contactMail: 'mailto:inquiry@aetheras.io',
+    twitterURL: 'https://twitter.com/aetheras_io',
+    contactMail: 'mailto:business@aetheras.io',
     recruitURL: 'https://www.104.com.tw/jobbank/custjob/index.php?r=cust&j=48524a733c3e466f383a426b40463e2193131312f483e527109j56&jobsource=checkc',
     whitePaperURL: 'https://www.aetheras.io/404',
     googleAnalyticsID: 'UA-142380558-1'
