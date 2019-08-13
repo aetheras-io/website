e06f3610448975db350269b5aeb1e5a8b9b38000
tlee21 <teddylee@aetheras.io>
2019-08-13 14:53:48 +0800

    Updates

---
+++ precache-manifest.060da85859c862bb9484cbe3db562691.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b245fb1dada2f23579ba1f4e9d792849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23653a3f6f4fb4a04829",
+    "url": "/static/css/2.cd57a8cd.chunk.css"
+  },
+  {
+    "revision": "15a045176b57d18efad7",
+    "url": "/static/css/main.30179b09.chunk.css"
+  },
+  {
+    "revision": "23653a3f6f4fb4a04829",
+    "url": "/static/js/2.50921d41.chunk.js"
+  },
+  {
+    "revision": "f1be633cfcef7804a9c6",
+    "url": "/static/js/3.58896b5c.chunk.js"
+  },
+  {
+    "revision": "f438acbf68589c1a1716",
+    "url": "/static/js/4.e2c4230d.chunk.js"
+  },
+  {
+    "revision": "15a045176b57d18efad7",
+    "url": "/static/js/main.1f706220.chunk.js"
+  },
+  {
+    "revision": "167ba690353621161f6d",
+    "url": "/static/js/runtime~main.43d93ed2.js"
+  }
+]);
