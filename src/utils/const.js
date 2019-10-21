149f8d7c6bfa6ac660136f3dd693b3a9966680f5
wuyuyu0222 <wuyuyu0222@gmail.com>
2019-10-21 17:13:25 +0800

    change recruit url to yourator

--- src/utils/const.js
+++ src/utils/const.js
@@ -3,7 +3,7 @@ const CONST = {
     twitterURL: 'https://twitter.com/aetheras_io',
     discordURL: 'https://discord.gg/R9TCtyh',
     contactMail: 'mailto:business@aetheras.io',
-    recruitURL: 'https://www.104.com.tw/jobbank/custjob/index.php?r=cust&j=48524a733c3e466f383a426b40463e2193131312f483e527109j56&jobsource=checkc',
+    recruitURL: 'https://www.yourator.co/companies/Aetheras',
     whitePaperURL: 'https://www.aetheras.io/404',
     googleAnalyticsID: 'UA-142380558-1'
 }
