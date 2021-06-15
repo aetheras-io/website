8c963e7bc6fdc26f0c40f891e26a54314e93ad63
tlee21 <teddylee@aetheras.io>
2021-06-15 14:57:00 +0800

    Updates

---
+++ precache-manifest.2a6153cf09589986b4279a35e1f7c1eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808155d13f198eecff0938ad24591302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d491306a517e17c7ef7",
+    "url": "/static/css/2.28dabb33.chunk.css"
+  },
+  {
+    "revision": "6559b34a9b4c481437fd",
+    "url": "/static/css/main.b5931715.chunk.css"
+  },
+  {
+    "revision": "5d491306a517e17c7ef7",
+    "url": "/static/js/2.2b3163c7.chunk.js"
+  },
+  {
+    "revision": "fa5a5f93ff61b141024a",
+    "url": "/static/js/3.2079419c.chunk.js"
+  },
+  {
+    "revision": "89917a3542570c763232",
+    "url": "/static/js/4.70d997fa.chunk.js"
+  },
+  {
+    "revision": "6559b34a9b4c481437fd",
+    "url": "/static/js/main.0be2b148.chunk.js"
+  },
+  {
+    "revision": "f197d0ee078025dc0ac9",
+    "url": "/static/js/runtime~main.8c39bbc3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d265706d6cff9a394500beeae2d61ca.js"
+  "/precache-manifest.2a6153cf09589986b4279a35e1f7c1eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
